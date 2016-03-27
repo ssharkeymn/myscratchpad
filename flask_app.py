1cfd4e1c1b95ffa134b8870dbf6fc7b766b3c01c
ssharkeymn <steve.sharkey@gmail.com>
2016-03-27 19:46:31 +0000

    scratchpad app with crsf protection

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,21 @@
-
-# A very simple Flask Hello World app for you to get started with...
-
 from flask import Flask, redirect, render_template, request, url_for
+from flask.ext.wtf import Form
+from wtforms import StringField
+from wtforms.validators import DataRequired
+
 from flask.ext.sqlalchemy import SQLAlchemy
+from sqlalchemy import desc
+from datetime import datetime
+
+from flask_wtf.csrf import CsrfProtect
+
+class MyForm(Form):
+    name = StringField('name', validators=[DataRequired()])
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
+app.secret_key = 'ljhqweroisyudfjweroincsdjkhwids.nkusyfd'
+CsrfProtect(app)
 
 SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
     username="ssharkey",
@@ -22,14 +32,25 @@ class Comment(db.Model):
     __tablename__ = "comments"
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(4096))
+    notedate = db.Column(db.DateTime)
 
 
 @app.route('/', methods=["GET","POST"])
 def index():
+    form = MyForm()
     if request.method == "GET":
-        return render_template("main_page.html", comments=Comment.query.all())
+        return render_template("main_page.html", comments=Comment.query.order_by(desc(Comment.notedate)).all())
 
-    comment = Comment(content=request.form["contents"])
-    db.session.add(comment)
-    db.session.commit()
+    if form.validate_on_submit:
+        comment = Comment(content=request.form["contents"], notedate=datetime.now())
+        db.session.add(comment)
+        db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/delete', methods=["POST"])
+def delete_entry():
+    form = MyForm()
+    if form.validate_on_submit:
+        Comment.query.filter(Comment.id == request.args.get('ID')).delete()
+        db.session.commit()
     return redirect(url_for('index'))
